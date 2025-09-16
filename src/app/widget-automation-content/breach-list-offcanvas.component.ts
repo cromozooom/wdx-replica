f5aec1c2ae37b96f8ebf80b42d82e561d0af43a6
Razvan Nicu <razvan.nicu@wealth-dynamix.com>
2025-09-16 22:46:55 +0100

    add offcanvas

--- src/app/widget-automation-content/breach-list-offcanvas.component.ts
+++ src/app/widget-automation-content/breach-list-offcanvas.component.ts
@@ -2,15 +2,16 @@ import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {
   NgbOffcanvas,
-  NgbOffcanvasRef,
   NgbOffcanvasModule,
+  NgbActiveOffcanvas
 } from "@ng-bootstrap/ng-bootstrap";
 import { BreachListItem } from "./widget-automation-content.models";
+import { BreachListGridComponent } from "./breach-list-grid.component";
 
 @Component({
   selector: "app-breach-list-offcanvas",
   standalone: true,
-  imports: [CommonModule, NgbOffcanvasModule],
+  imports: [CommonModule, NgbOffcanvasModule, BreachListGridComponent],
   template: `
     <div class="offcanvas-header">
       <h5 class="offcanvas-title">Breach List</h5>
@@ -18,16 +19,14 @@ import { BreachListItem } from "./widget-automation-content.models";
         type="button"
         class="btn-close"
         aria-label="Close"
-        (click)="close()"
+        (click)="activeOffcanvas.close()"
       ></button>
     </div>
     <div class="offcanvas-body">
       <ng-container *ngIf="breachList?.length; else noBreaches">
-        <ul class="list-group">
-          <li class="list-group-item" *ngFor="let breach of breachList">
-            {{ breach.details }}
-          </li>
-        </ul>
+        <app-breach-list-grid
+          [breachList]="breachList || []"
+        ></app-breach-list-grid>
       </ng-container>
       <ng-template #noBreaches>
         <div class="text-muted">No breaches found.</div>
@@ -37,22 +36,5 @@ import { BreachListItem } from "./widget-automation-content.models";
 })
 export class BreachListOffcanvasComponent {
   @Input() breachList: BreachListItem[] | null = null;
-  private offcanvasRef: NgbOffcanvasRef | null = null;
-
-  constructor(private offcanvas: NgbOffcanvas) {}
-
-  open(breachList: BreachListItem[]) {
-    this.breachList = breachList;
-    this.offcanvasRef = this.offcanvas.open(BreachListOffcanvasComponent, {
-      position: "end",
-      panelClass: "offcanvas-width-md",
-    });
-    (
-      this.offcanvasRef.componentInstance as BreachListOffcanvasComponent
-    ).breachList = breachList;
-  }
-
-  close() {
-    this.offcanvasRef?.close();
-  }
+  constructor(public activeOffcanvas: NgbActiveOffcanvas) {}
 }
