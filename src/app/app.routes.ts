ddafc801dfd26cbfdca7a66254a3be5426664334
cromozooom <cromozooom@gmail.com>
2025-09-26 03:17:32 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,21 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 
 export const routes: Routes = [
   {
-    path: "",
-    component: DashboardComponent,
+    path: 'dashboard',
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+      {
+        path: ':type',
+        component: DashboardComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
 ];
